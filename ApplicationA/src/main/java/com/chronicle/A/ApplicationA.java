23d62918916dd48e974408da23e14711ff1a4dad
Jan <jan.witek88@gmail.com>
2022-02-18 10:04:00 +0100

    fixed reading from queue

--- ApplicationA/src/main/java/com/chronicle/A/ApplicationA.java
+++ ApplicationA/src/main/java/com/chronicle/A/ApplicationA.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
+//@SpringBootApplication(exclude = {ServletWebServerFactoryAutoConfiguration.class,
+//        WebMvcAutoConfiguration.class})
 @SpringBootApplication
-@PropertySources({
-        @PropertySource("classpath:application.properties")
-})
+//@PropertySources({
+//        @PropertySource("classpath:application.properties")
+//})
 public class ApplicationA {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationA.class, args);
